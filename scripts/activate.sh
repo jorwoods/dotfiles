55050de84ba5a24ab1542198ddadf68f706da8ba
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2023-11-23 10:14:16 -0600

    feat: add shortcut for activating venvs

---
+++ scripts/activate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Find the virtual environment in the current directory
+VENV=$(find . -type d -iname ".venv")
+
+# Check if the virtual environment exists
+if [ -z "$VENV" ]; then
+    echo "No virtual environment found in the current directory."
+    exit 1
+fi
+
+# Activate the virtual environment
+source $VENV/bin/activate
