3e2b6e9abdee10a0833be7a2fd7b192176042ef1
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2023-11-23 21:38:26 -0600

    feat: add descriptions to custom keymaps

--- config/nvim/lua/jor/remap.lua
+++ config/nvim/lua/jor/remap.lua
@@ -1,36 +1,33 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
--- Gives a different hotkey to copy to the system clipboard
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc = 'Open project folder view' })
 
-vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = 'Navigate files tracked in git' })
 vim.keymap.set("n", "<leader>ps", function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+end, { desc = 'Regex search within git tracked files' })
 
 -- Visual mode. Allows J and K to move selected blocks up and down.
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Move block down' })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Move block up' })
 
 -- Keeps cursor in place when using "J" to append next line to current
-vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "J", "mzJ`z", { desc = 'Append next line to current' })
 
 -- Keeps cursor in the middle of the screen when moving with half page jumps
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = 'Move screen down by half page' })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = 'Move screen up by half page' })
 
 -- Replace with word copied, but send cut text into the void register
-vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("x", "<leader>p", "\"_dP", { desc = 'Replace with copied, sending cut text to void' })
 
 -- Gives a different hotkey to copy to the system clipboard
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
+-- Gives a different hotkey to copy to the system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y", { desc = 'Yank to system clipboard' })
+vim.keymap.set("v", "<leader>y", "\"+y", { desc = 'Yank to system clipboard' })
+vim.keymap.set("n", "<leader>Y", "\"+Y", { desc = 'Yank to system clipboard' })
 
 -- Delete to void register
-vim.keymap.set("x", "<leader>d", "\"_d")
+vim.keymap.set("x", "<leader>d", "\"_d", { desc = 'Delete and send to void' })
 
 -- Ignore Q
 vim.keymap.set("n", "Q", "<nop>")
