4b32d6753724051cdd26b62ab9dd69082e5e99e4
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2023-12-02 05:16:32 -0600

    merge luasnip and dap

--- config/nvim/lua/jor/lazy.lua
+++ config/nvim/lua/jor/lazy.lua
@@ -72,6 +72,16 @@ require("lazy").setup({
     },
   },
 
+
+  -- Debugging
+  { 'mfussenegger/nvim-dap' },
+  { 'rcarriga/nvim-dap-ui' },
+  { 'theHamsta/nvim-dap-virtual-text' },
+  { 'nvim-telescope/telescope-dap.nvim' },
+
+  -- Language specific DAP configurations
+  { 'mfussenegger/nvim-dap-python' },
+
   -- NVIM niceties
   { 'folke/which-key.nvim', opts = {} },
   { 'nvim-lualine/lualine.nvim', 
