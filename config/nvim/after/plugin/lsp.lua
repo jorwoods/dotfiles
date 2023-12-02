f6bfb8596186d57fc858e0e02e9ea62e33ddb0b6
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2023-12-02 13:37:29 -0600

    feat: completions now working

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -1,21 +1,4 @@
 require("mason").setup()
--- local lsp = require('lsp-zero')
--- lsp.on_attach(function(client, bufnr)
--- 	local opts = {buffer = bufnr, remap = false}
-
--- 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
--- 	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
--- 	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
--- 	vim.keymap.set("n", "<leader>vd", function() vim.lsp.buf.open_float() end, opts)
--- 	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
--- 	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
--- 	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
--- 	vim.keymap.set("n", "[d", function() vim.lsp.buf.goto_next() end, opts)
--- 	vim.keymap.set("n", "]d", function() vim.lsp.buf.goto_prev() end, opts)
--- 	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-
--- end)
-
 local on_attach = function(_, bufnr)
     local nmap = function(keys, func, desc)
         if desc then
@@ -28,18 +11,18 @@ local on_attach = function(_, bufnr)
     nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
     nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-    telescope = require('telescope.builtin')
+    local telescope = require('telescope.builtin')
     nmap('gd', telescope.lsp_definitions, '[G]oto [D]efinition')
     nmap('gr', telescope.lsp_references, '[G]oto [R]eferences')
     nmap('gi', telescope.lsp_implementations, '[G]oto [I]mplementations')
     nmap('<leader>D', telescope.lsp_type_definitions, 'Type [D]efinitions')
     nmap('<leader>ws', telescope.lsp_workspace_symbols, '[W]orkspace [S]ymbols')
     nmap('<leader>gn', vim.diagnostic.goto_next, '[G]oto [N]ext Diagnostic')
-    nmap('<leader>gp', vim.diagnostic.goto_previous, '[G]oto [N]ext Diagnostic')
+    nmap('<leader>gp', vim.diagnostic.goto_prev, '[G]oto [P]revious Diagnostic')
 --  Need to find a different mapping for this due to conflict with delete keymap
 --    nmap('<leader>dl', telescope.lsp_document_diagnostics, '[D]ocument [L]inting')
 
-    
+
     -- See `:help K` for more info
     nmap('K', vim.lsp.buf.hover, 'Show [D]ocumentation')
     nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
@@ -97,5 +80,56 @@ mason_lspconfig.setup_handlers {
     end
 }
 
+-- Configure auto completions with
+-- [[ Configure nvim-cmp ]]
+-- See `:help cmp` for more info
+
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+require('luasnip.loaders.from_vscode').lazy_load()
+luasnip.config.setup {}
+
+cmp.setup {
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
+    },
+    mapping = {
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        },
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+    },
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+--        { name = 'buffer' },
+    },
+}
 
 
