53572aacb6ebb2608afa7065d17b978516b3ab53
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2024-05-30 22:32:50 -0500

    feat: update keymaps

--- config/nvim/lua/jor/remap.lua
+++ config/nvim/lua/jor/remap.lua
@@ -1,38 +1,49 @@
 local builtin = require('telescope.builtin')
+local set = vim.keymap.set
 
-vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = 'Navigate files tracked in git' })
-vim.keymap.set("n", "<leader>ps", function()
+    set("n", "<C-p>", builtin.git_files, { desc = 'Navigate files tracked in git' })
+set("n", "<leader>ps", function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end, { desc = 'Regex search within git tracked files' })
 
 -- Visual mode. Allows J and K to move selected blocks up and down.
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Move block down' })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Move block up' })
+set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Move block down' })
+set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Move block up' })
 
 -- Keeps cursor in place when using "J" to append next line to current
-vim.keymap.set("n", "J", "mzJ`z", { desc = 'Append next line to current' })
+set("n", "J", "mzJ`z", { desc = 'Append next line to current' })
 
 -- Keeps cursor in the middle of the screen when moving with half page jumps
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = 'Move screen down by half page' })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = 'Move screen up by half page' })
+set("n", "<C-d>", "<C-d>zz", { desc = 'Move screen down by half page' })
+set("n", "<C-u>", "<C-u>zz", { desc = 'Move screen up by half page' })
 
 -- Replace with word copied, but send cut text into the void register
-vim.keymap.set("x", "<leader>pr", "\"_dP", { desc = 'Replace with copied, sending cut text to void' })
+set("x", "<leader>pr", "\"_dP", { desc = 'Replace with copied, sending cut text to void' })
 
 -- Gives a different hotkey to copy to the system clipboard
 -- Gives a different hotkey to copy to the system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", "\"+y", { desc = 'Yank to system clipboard' })
-vim.keymap.set("n", "<leader>Y", "\"+Y", { desc = 'Yank to system clipboard' })
+set({ "n", "v" }, "<leader>y", "\"+y", { desc = 'Yank to system clipboard' })
+set("n", "<leader>Y", "\"+Y", { desc = 'Yank to system clipboard' })
 
 -- Delete to void register
-vim.keymap.set("n", "<leader>dd", "\"_d", { desc = 'Delete and send to void' })
+set("n", "<leader>dd", "\"_d", { desc = 'Delete and send to void' })
+set("n", "<leader>cc", "\"_c", { desc = 'Change and send to void' })
 
 -- Paste from system clipboard
-vim.keymap.set("n", "<leader>pp", "\"+p", { desc = 'Paste from system clipboard' })
-
+set("n", "<leader>pp", "\"+p", { desc = 'Paste from system clipboard' })
 
 -- Ignore Q
-vim.keymap.set("n", "Q", "<nop>")
+set("n", "Q", "<nop>")
 
 -- golang error handling
-vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<ESC>Oreturn err<ESC>",{ desc = "Insert golang error handler" })
+set("n", "<leader>ee", "oif err != nil {<CR>}<ESC>Oreturn err<ESC>",{ desc = "Insert golang error handler" })
+
+-- These mappings control the size of splits (height/width)
+set("n", "<M-,>", "<c-w>5<", { desc = "Increase split width" })
+set("n", "<M-.>", "<c-w>5>", { desc = "Decrease split width" })
+set("n", "<M-t>", "<C-W>+", { desc = "Increase split height" })
+set("n", "<M-s>", "<C-W>-", { desc = "Decrease split height" })
+
+-- lua execution
+set("n", "<leader>x", "<cmd>.lua<CR>", { desc = 'Execute lua line' })
+set("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = 'Execute lua line' })
