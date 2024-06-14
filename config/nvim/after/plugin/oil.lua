e6ce671a481ac004fbfbb16cd4ade27fa8d2e4e2
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2024-06-14 13:50:01 -0500

    feat: configure oil.nvim

--- config/nvim/after/plugin/oil.lua
+++ config/nvim/after/plugin/oil.lua
@@ -1,6 +1,21 @@
 local oil = require("oil")
 
 oil.setup({
+    default_file_explorer = true,
+    view_options = {
+        show_hidden = true,
+        natural_order = true,
+        is_always_hidden = function(name, _)
+            local ignores = {'.git', '..', '__pycache__',}
+            for _,v in ipairs(ignores) do
+                if name == v then
+                    return true
+                end
+            end
+            return false
+        end
+    }
 
 })
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Oil , { desc = 'Open project folder view' })
