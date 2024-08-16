27cb04baba52b4d35832fb7bc4d2d7e990f28543
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2024-08-15 22:13:59 -0500

    feat: switch to flash over leap

--- config/nvim/lua/jor/lazy.lua
+++ config/nvim/lua/jor/lazy.lua
@@ -21,7 +21,21 @@ require("lazy").setup({
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
     { 'ThePrimeagen/harpoon', },
-    { 'ggandor/leap.nvim', },
+    -- { 'ggandor/leap.nvim', },
+    {
+      "folke/flash.nvim",
+      event = "VeryLazy",
+      ---@type Flash.Config
+      opts = {},
+      -- stylua: ignore
+      keys = {
+        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+        { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+        { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+        { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      },
+    },
     {
         'SmiteshP/nvim-navbuddy',
         dependencies = {
