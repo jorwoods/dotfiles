b5531777e2ec8313c0615f7a100db7ca9ebe9977
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2024-08-16 10:41:52 -0500

    feat: add codesnap plugin

--- config/nvim/lua/jor/lazy.lua
+++ config/nvim/lua/jor/lazy.lua
@@ -150,6 +150,9 @@ require("lazy").setup({
     -- Paste images from clipboard
     { 'HakonHarnes/img-clip.nvim', },
 
+    -- Screenshot code to clipboard
+    { "mistricky/codesnap.nvim", build = "make" },
+
       {
     "CopilotC-Nvim/CopilotChat.nvim",
     opts = {
