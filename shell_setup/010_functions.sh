11f1b27d0e32811e60fc0d39548bf8329712177a
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2024-08-21 21:54:18 -0500

    feat: fzf git log show

--- shell_setup/010_functions.sh
+++ shell_setup/010_functions.sh
@@ -4,3 +4,17 @@ function fcd() {
         cd $dir_
     fi
 }
+
+# fshow - git commit browser
+fshow() {
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+      --preview \
+           'f() { set -- $(echo -- "$@" | grep -o "[a-f0-9]\{7\}"); [ $# -eq 0 ] || git show --color=always $1 ; }; f {}' \
+      --bind "ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+FZF-EOF"
+}
