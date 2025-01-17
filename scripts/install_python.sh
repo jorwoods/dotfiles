4c2bfd8e6eab8caffd4f05b81b5f09dab8f46567
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2025-01-17 14:59:09 -0600

    chore: double quote variables in shell scripts

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -7,54 +7,53 @@ sudo apt-get install build-essential gdb lcov pkg-config \
       libncurses5-dev libreadline6-dev libsqlite3-dev libssl-dev \
       lzma lzma-dev tk-dev uuid-dev zlib1g-dev
 
-if [ ! -d "$HOME/programs" ]; then
-    mkdir "$HOME/programs"
+if [[ ! -d "${HOME}/programs" ]]; then
+    mkdir "${HOME}/programs"
 fi
 
-if [ ! -d "$HOME/.local/bin" ]; then
-    mkdir "$HOME/.local/bin"
+if [[ ! -d "${HOME}/.local/bin" ]]; then
+    mkdir "${HOME}/.local/bin"
 fi
-pushd "$HOME/programs"
+pushd "${HOME}/programs"
 
 base_url="https://www.python.org/ftp/python"
-version=$(curl -L $base_url | grep -oP "(3\.[0-9]+\.[0-9]+)" | sort -u | fzf --prompt="What version of Python do you want to install? ")
-echo "\n\nInstalling Python $version from source."
+version=$(curl -L "${base_url}" | grep -oP "(3\.[0-9]+\.[0-9]+)" | sort -u | fzf --prompt="What version of Python do you want to install? ")
+printf "\n\nInstalling Python %s from source." "${version}"
 
-if [ ! -d "$HOME/programs/Python-$version" ];
-then
-    echo "Downloading Python $version"
-    wget "$base_url/$version/Python-$version.tgz"
-    tar -xvf "Python-$version.tgz"
-    rm "Python-$version.tgz"
-    pushd ./Python-$version
+if [[ ! -d "$HOME/programs/Python-${version}" ]]; then
+    echo "Downloading Python ${version}"
+    wget "${base_url}/${version}/Python-${version}.tgz"
+    tar -xvf "Python-${version}.tgz"
+    rm "Python-${version}.tgz"
+    pushd "./Python-${version}"
 
-    echo "Configuring Python $version"
-    ./configure --prefix=$HOME/programs/python-$version --enable-optimizations
-    echo "Building Python $version"
-    make -s -j$(nproc)
-    echo "Installing Python $version"
+    echo "Configuring Python ${version}"
+    ./configure --prefix="${HOME}/programs/python-${version}" --enable-optimizations
+    echo "Building Python ${version}"
+    make -s -j"$(nproc)"
+    echo "Installing Python ${version}"
     sudo make altinstall
     popd
 else
-    echo "Python $version already installed."
+    echo "Python ${version} already installed."
 fi
 
-short_ver=$(echo $version | grep -oP "3\.\d+")
-py_link="$HOME/.local/bin/python$short_ver"
-if [ ! -f $py_link ];
+short_ver=$(echo "${version}" | grep -oP "3\.\d+")
+py_link="$HOME/.local/bin/python${short_ver}"
+if [[ ! -f "${py_link}" ]];
 then
-    ln -s "$HOME/programs/Python-$version/python" "$py_link"
+    ln -s "$HOME/programs/Python-${version}/python" "${py_link}"
 else
-    linked_bin=$(realpath "$py_link")
-    linked_ver=$($py_link --version)
+    linked_bin=$(realpath "${py_link}")
+    linked_ver=$("${py_link}" --version)
 
-    echo "A link for Python $short_ver already exists and points to $linked_bin ($linked_ver)."
+    echo "A link for Python ${short_ver} already exists and points to ${linked_bin} (${linked_ver})."
     read -p "Do you want to overwrite this link? (Y/n) " -n 1 -r
     echo    # move to a new line
-    if [[ -z $REPLY || $REPLY =~ ^[Yy]$ ]]
+    if [[ -z "${REPLY}" || "${REPLY}" =~ ^[Yy]$ ]]
     then
-        rm "$py_link"
-        ln -s "$HOME/programs/Python-$version/python" "$py_link"
+        rm "${py_link}"
+        ln -s "$HOME/programs/Python-${version}/python" "${py_link}"
         echo "Link overwritten."
     else
         echo "Link not overwritten."
--- shell_setup/040_python.sh
+++ shell_setup/040_python.sh
@@ -1,35 +1,44 @@
+sym_link_python(){
+    local local_bin
+    local_bin="${HOME}/.local/bin"
 
-local_bin="$HOME/.local/bin"
-python_folders=$(find "$HOME/programs" -type d -maxdepth 1 -name "Python-3.*" 2>/dev/null)
-for folder in $python_folders; do
-	version=$(echo $folder | grep -oP "Python-\K3\.\d+" )
-	if [ -x "$folder/python" ]; then
-    if [ ! -f "$local_bin/python$version" ]; then
-      ln -s "$folder/python" "$local_bin/python$version"
-    fi
-	fi
-done
-# Python will put binaries for pip and other tools in the ~/.local/bin folder
+    local versions
+    versions=()
 
-py3=$(\find "$local_bin" -name "python3*" | sort -Vr | head -1)
-rm "$local_bin/python3" 2>/dev/null; ln -s "$py3" "$local_bin/python3"
+    for folder in "$HOME/programs"/Python*; do
+        if [[ ! -d "${folder}" ]]; then
+            continue
+        fi
 
-if [[ ! -d "$HOME/.virtualenvs" ]]; then
-    mkdir "$HOME/.virtualenvs"
-fi
+        local version
+        version="${folder##*-}"
+        versions+=("${version}")
+        local short_ver
+        short_ver="${version%.*}"
 
-if [[ ! -d "$HOME/.virtualenvs/debugpy" ]]; then
-    pushd "$HOME/.virtualenvs"
-    python3 -m venv debugpy
-    ./debugpy/bin/python -m pip install debugpy
-    popd
-fi
+        if [[ -x "${folder}/python" ]]; then
+            if [[ ! -e "${local_bin}/python${version}" ]]; then
+                ln -sf "${folder}/python" "${local_bin}/python${version}"
+            fi
+
+            if [[ ! -e "${local_bin}/python/${short_ver}" ]]; then
+                ln -sf "${folder}/python" "${local_bin}/python${short_ver}"
+            fi
+        fi
+    done
+
+    local latest
+    latest=$(printf "%s\n" "${versions[@]}" | sort -V | tail -n1)
+
+    ln -sf "${local_bin}/python${latest}" "${local_bin}/python3"
+}
+
 
 select_python() {
     # Iterate over each directory in the PATH variable
-    for dir in $(echo $PATH | tr ":" "\n" | grep -v /mnt/c/); do
+    for dir in $(echo "${PATH}" | tr ":" "\n" | grep -v /mnt/c/); do
         # Find all executable files starting with 'python' in the current directory
-        find $dir -name 'python*' -executable -maxdepth 1
+        find "$dir" -name 'python*' -executable -maxdepth 1
     done 2>/dev/null |
     # Exclude 'python*-config' from the output
     grep -v config |
@@ -44,47 +53,64 @@ venv() {
     venv_name=${1:-.venv}
     local py
     py=$(select_python)
-    if [[ -z $py ]]; then
+    if [[ -z "${py}" ]]; then
         echo "No python version selected"
         return 1
     fi
     local version
-    version=$($py --version)
+    version=$("${py}" --version)
     local folder
-    folder=$(basename $PWD)
-    if [[ -d $venv_name ]]; then
-        echo "$venv_name already exists"
+    folder=$(basename "${PWD}")
+    if [[ -d "${venv_name}" ]]; then
+        echo "${venv_name} already exists"
         return 1
     fi
-    echo "Creating virtual environment for $version in $venv_name"
-    $py -m venv --prompt "$folder" $venv_name
+    echo "Creating virtual environment for ${version} in ${venv_name}"
+    "$py" -m venv --prompt "${folder}" "${venv_name}"
     touch .gitignore > /dev/null 2>&1
-    if ! grep -q $venv_name .gitignore; then
-        echo "Adding $venv_name/ to .gitignore"
-        echo "$venv_name/" >> .gitignore
+    if ! grep -q "${venv_name}" .gitignore; then
+        echo "Adding ${venv_name}/ to .gitignore"
+        echo "${venv_name}/" >> .gitignore
     fi
-    echo "Upgrading $venv_name's pip"
-    "$PWD/$venv_name/bin/python" -m pip install --upgrade pip > /dev/null
-    echo "$venv_name/ created. Activate with 'source $venv_name/bin/activate'"
+    echo "Upgrading ${venv_name}'s pip"
+    "$PWD/${venv_name}/bin/python" -m pip install --upgrade pip > /dev/null
+    echo "${venv_name}/ created. Activate with 'source ${venv_name}/bin/activate'"
 }
 
 activate() {
     local venvs
     venvs=$(fd --hidden --no-ignore --max-depth 2 pyvenv.cfg)
     local venv_count
-    venv_count=$(echo $venvs | wc -l)
-    if [[ $venv_count -eq 0 ]]; then
+    venv_count=$(echo "${venvs}" | wc -l)
+    if [[ "${venv_count}" -eq 0 ]]; then
         echo "No virtual environments found"
         return 1
     fi
-    if [[ $venv_count -eq 1 ]]; then
-        source $(dirname $venvs)/bin/activate
+    if [[ "${venv_count}" -eq 1 ]]; then
+        source $(dirname "${venvs}")/bin/activate
         return 0
     fi
     local venv_name
-    venv_name=$(echo $venvs | xargs -L1 dirname | fzf --prompt="Select virtual environment:")
+    venv_name=$(echo "$venvs" | xargs -L1 dirname | fzf --prompt="Select virtual environment:")
 
-    if [[ -n $venv_name ]]; then
-        source $venv_name/bin/activate
+    if [[ -n "${venv_name}" ]]; then
+        source "${venv_name}/bin/activate"
     fi
 }
+
+main() {
+sym_link_python > /dev/null
+
+if [[ ! -d "$HOME/.virtualenvs" ]]; then
+    mkdir "$HOME/.virtualenvs"
+fi
+
+if [[ ! -d "$HOME/.virtualenvs/debugpy" ]]; then
+    pushd "$HOME/.virtualenvs" || exit
+    python3 -m venv debugpy
+    ./debugpy/bin/python -m pip install debugpy
+    popd || exit
+fi
+}
+
+main
