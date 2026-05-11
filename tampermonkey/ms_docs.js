706aa51f40f8f0d881bc14d7f5a964de8ba9850b
Jordan Woods <jorwoods@users.noreply.github.com>
2026-05-11 16:02:57 -0500

    fix: msft tampermonkey
    Updated heading selector and added overflow style.

--- tampermonkey/ms_docs.js
+++ tampermonkey/ms_docs.js
@@ -1,6 +1,6 @@
 let replace = document.querySelector('#affixed-right-container');
 
-let headings = Array.from(document.querySelectorAll('div.content h2')).filter(
+let headings = Array.from(document.querySelectorAll('div.content .heading-anchor')).filter(
     e => !e.id.includes('in-this-article')
 ).map(
     h => `<li><a href="#${h.id}">${h.innerText}</a></li>`
@@ -8,3 +8,4 @@ let headings = Array.from(document.querySelectorAll('div.content h2')).filter(
 
 let links = headings.join('\n');
 replace.innerHTML = `<ol>${links}</ol>`;
+replace.style.overflowY = 'auto';
